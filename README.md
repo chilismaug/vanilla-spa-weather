f9832300782fba27ae69d61cf0d1de837e765922
chilismaug <charles.ross.oh@gmail.com>
2021-01-21 21:55:54 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Openweather SPA hack
+
+This is a vanilla JS single page web application to search openweather REST API 
+
+## Usage
+1. Go to the page
+2. Enter the city.
+3. Weather data will be loaded via an AJAX call.
+
+
+## Author
+Copy source  https://github.com/raajnadar/github-user-card
+ - [Rajendran Nadar](https://raajnadar.in)
+
+## License
+
+This project is licensed under - MIT
---
+++ js/main.js
@@ -0,0 +1,67 @@
+document.onreadystatechange = function () {
+	
+
+	// Fetch method
+	function fetchData() {
+		// Get cityname from input box
+		var cityname = document.querySelector('#cityname').value;
+		
+		// Ajax call
+        const API_KEY = '40d0c5223044f01a0f0142846dc203d3'
+        const API_URL = ('http://api.openweathermap.org/data/2.5/weather?&mode=json&units=metric')
+        //   in python we ask it       data = requests.get(API_URL.format(city, API_KEY)).json()
+      //  q={} city
+      //  &appid={} api key
+
+		var request = new XMLHttpRequest();
+		request.open('GET',  API_URL +'&q=' +cityname+ '&appid='+ API_KEY ) ;
+		request.onreadystatechange = function () {
+			if (request.readyState == 4 ) {
+				if (request.status === 200) {
+					var data = JSON.parse(request.responseText);
+                    var templatedCard =`
+                         <div class="data-badge">City Name ${ data.name }</div> 
+                         <div class="data-badge">Country ${ data["sys"]["country"]}</div>
+                         <div class="data-badge">TZone ${ data.timezone }</div>
+                                                  
+                         <p><hr>
+                         <div class="data-badge">Temp ${ data["main"]["temp"] } °C</div>   
+                         <div class="data-badge"> ${ data["weather"][0]["main"] }</div>   
+                         <div class="data-badge">Vis ${ data.visibility }</div>
+                         <div class="data-badge">Clouds ${ data["clouds"]["all"]}</div>                         
+                         <div>            
+                             <button class="btn btn-blue" id="searchAgain">Search Again</button>
+                         </div>
+                      </div>`;
+					if (data.message !== 'Not Found') {
+						searchForm.style.display = "none"
+						result.innerHTML = templatedCard;
+					}
+					
+					var searchAgain = document.querySelector('#searchAgain');
+			
+					searchAgain.addEventListener('click', function () {
+						searchForm.style.display = "block"
+
+						result.innerHTML = ''
+						document.querySelector('#cityname').value = ''
+					});
+				}  else {
+					alert('No users found!')
+				}
+			}
+		}
+		request.send();
+	}
+	
+	/* wait for the contents to load */
+	if (document.readyState === 'complete') {
+		var result = document.querySelector('#result');
+		var searchForm = document.querySelector('#search-form');
+		
+		searchForm.addEventListener('submit', function (e) {
+			e.preventDefault()
+			fetchData()
+		});
+	}
+}
